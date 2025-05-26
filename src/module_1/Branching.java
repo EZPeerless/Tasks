1e987ba9f673eb6705817001cebd6c9734730db3
EZPeerless <ezpeerless@gmail.com>
2025-05-26 20:07:24 +0700

    Clear code

--- src/module_1/Branching.java
+++ src/module_1/Branching.java
@@ -5,57 +5,58 @@ import java.text.DecimalFormat;
 public class Branching {
 
     public static void main(String[] args) {
-        task1(30, 60);
-        task2(98, 300, 56, 90);
-        task3(1, 1, 2, 2, 3, 3);
-        task4(15, 20, 25, 12, 6.5);
-        task5(-3.3456);
+        System.out.println(task1(30, 60));
+        System.out.println(task2(98, 300, 56, 90));
+        System.out.println(task3(1, 1, 2, 2, 3, 3));
+        System.out.println(task4(15, 20, 25, 12, 6.5));
+        System.out.println(task5(-3.3456));
     }
 
-    public static void task1(double a, double b) {
+    public static String task1(double a, double b) {
         double result = a + b;
         double triangle = 180;
         double c = triangle - result;
+        String message;
         if (a + b >= triangle || c <= 0) {
-            System.out.println("Task 1: Треугольника не существует");
+            message = ("Task 1: Треугольника не существует");
         } else {
-            System.out.print("Task 1: Треугольник существует");
+            message = ("Task 1: Треугольник существует");
             if (a == 90 || b == 90 || c == 90) {
-                System.out.println(", он прямоугольный");
+                message+= ( ", он прямоугольный");
             } else {
-                System.out.println();
+                message = ("");
             }
         }
+        return message;
     }
 
-    public static void task2(double a, double b, double c, double d) {
-        /* System.out.println(Math.max(Math.min(a,b),Math.min(c,d))); */ // более простой и логичный вариант через Math.
+    public static String task2(double a, double b, double c, double d) {
         double fline = Math.min(a, b);
         double sline = Math.min(c, d);
-        String answer = (fline > sline) ? "Task 2: min(a,b)" : "Task 2: min(c:d)";
-        System.out.println(answer);
+        return (fline > sline) ? "Task 2: min(a,b)" : "Task 2: min(c:d)";
+
     }
 
-    public static void task3(double x1, double y1, double x2, double y2, double x3, double y3) {
+    public static String task3(double x1, double y1, double x2, double y2, double x3, double y3) {
         double x = (-x1) / (x2 - x1);
         double y = (-y1) / (y2 - y1);
         double xy = y - x;
         if (x3 - y3 == xy) {
-            System.out.println("Task 3: Точки лежат на одной прямой");
+            return ("Task 3: Точки лежат на одной прямой");
         } else {
-            System.out.println("Task 3: Точки не лежат на одной прямой");
+            return ("Task 3: Точки не лежат на одной прямой");
         }
     }
 
-    public static void task4(double A, double B, double x, double y, double z) {
+    public static String task4(double A, double B, double x, double y, double z) {
         if (A > x && B > y || A > x && B > z || A > y && B > z) {
-            System.out.println("Task 4: Кирпич проходит");
+            return ("Task 4: Кирпич проходит");
         } else {
-            System.out.println("Task 4: Кирпич не проходит");
+            return ("Task 4: Кирпич не проходит");
         }
     }
 
-    public static void task5(double x) {
+    public static String task5(double x) {
         double F1;
         double F2;
         F1 = Math.pow(x, 2)- 3 * x + 9 ;
@@ -65,9 +66,9 @@ public class Branching {
         String res2 = decimalFormat.format(F2);
 
         if (x <= 3) {
-            System.out.println("Task 5: F(x)=" + res1);
+            return ("Task 5: F(x)=" + res1);
         } else {
-            System.out.println("Task 5: F(x)=" + res2);
+            return ("Task 5: F(x)=" + res2);
         }
     }
 }
--- src/module_1/Linear.java
+++ src/module_1/Linear.java
@@ -2,67 +2,57 @@ package module_1;
 
 public class Linear {
     public static void main(String[] args) {
-        task1(1, 3, 8);
-        task2(7, 8, 2);
-        task3(5, 9);
-        task4(767.998);
-        task5(125753434);
-        task6(2, -1,0.5);
+        System.out.println("Task 1: " + task1(2,3,8));
+        System.out.println("Task 2: " + task2(7, 8, 2));
+        System.out.println("Task 3: " + task3(5, 9));
+        System.out.println("Task 4: " + task4(767.998));
+        System.out.println("Task 5: " + task5(125753434));
+        System.out.println(task6(2, -1,0.5));
     }
 
     public static void method1(int number) {
         System.out.println("Hello" + number);
     }
 
-    public static void task1(double a, double b, double c) {
-        double z = ((a - 3) * b / 2) + c;
-        System.out.println("Task 1: " + z);
+    public static double task1(double a, double b, double c) {
+        return  ((a - 3) * b / 2) + c;
     }
 
-    public static void task2(double a, double b, double c) {
-        double res1 = Math.sqrt(Math.pow(b, 2) + 4 * a * c); // Результат вычеслений под корнем
-        double res2 = b + res1; // Знаменатель дроби
-        double res3 = 2 * a; // Делитель дроби
-        double res4 = res2 / res3; // Трансформация дроби в число
+    public static double task2(double a, double b, double c) {
+        double res1 = Math.sqrt(Math.pow(b, 2) + 4 * a * c);
+        double res2 = b + res1;
+        double res3 = 2 * a;
+        double res4 = res2 / res3;
         double res5 = Math.pow(a, 3) * c + Math.pow(b, -3);
-        double res6 = res4 - res5;
-        // System.out.println((b+res1)/2*a); // Вывод результата пристрелки
-        System.out.println("Task 2: " + res6);
-        /* double step = Math.pow(b, 4);
-        System.out.println("Степень числа " + step);
-        System.out.println("Корень из " + Math.sqrt(step)); */ // Чисто пристрелка
+        return res4 - res5;
     }
 
-    public static void task3(double x, double y) {
-        double all = ((Math.sin(x) + Math.cos(y)) / (Math.cos(x) - Math.sin(y))) * (Math.tan(x * y));
-        System.out.println("Task 3: " + all);
+    public static double task3(double x, double y) {
+        return ((Math.sin(x) + Math.cos(y)) / (Math.cos(x) - Math.sin(y))) * (Math.tan(x * y));
     }
 
-    public static void task4(double R) {
-        double x = (int) R; // приводим переменную R к целочисленному значению
-        double escape = (R - x) * 1000 + (x / 1000); // сам процесс (^_^)
-        String result = String.format("%.3f", escape); // оганичиваем колличество знаков после точки до 3х
-        System.out.println("Task 4: " + result);
+    public static String task4(double R) {
+        double x = (int) R;
+        double escape = (R - x) * 1000 + (x / 1000);
+        return String.format("%.3f", escape);
     }
 
-    private static void task5(int T) {
-        int time = T / 1000; // Переводим миллисекнудны в секунды
+    private static String task5(int T) {
+        int time = T / 1000;
         int hours = time / 3600;
         int minute = (time - (hours * 3600)) / 60;
         int seconds = (time - (hours * 3600)) - minute * 60;
-        System.out.println("Task 5: " + hours + "ч " + minute + "мин " + seconds + "с");
+        return (hours + "ч " + minute + "мин " + seconds + "с");
     }
 
-    private static void task6(double x, double y, double size) {
-
-        // переменная size - устанавливает шаг сетки координат
+    private static String task6(double x, double y, double size) {
 
         if (y >= -3*size && y < 0 && x >= -4*size && x <= 4*size) {
-            System.out.println("Task 5: " + true);
+            return ("Task 6: " + true);
         } else if (y >= 0 && y <= 4*size && x >= -2*size && x <= 2*size) {
-            System.out.println("Task 5: " + true);
+            return ("Task 6: " + true);
         } else {
-            System.out.println("Task 5: " + false);
+            return ("Task 6: " + false);
         }
     }
 }
