00471bd515798015d6198d3ca4d6b2ac16b060dd
EZPeerless <ezpeerless@gmail.com>
2025-08-02 23:34:27 +0700

    Complete task 2 & 3 in Array.java

--- src/module_2/Array.java
+++ src/module_2/Array.java
@@ -1,11 +1,14 @@
 package module_2;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Array {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Task 1: Ответ: " + task1());
+        System.out.println("Task 2: Ответ: " + task2());
+        System.out.println("Task 3: Ответ: " + task3());
     }
 
     /*
@@ -35,6 +38,70 @@ public class Array {
             }
         }
         return results;
+    }
+
+    /*
+     Задание 2:
+     Дана последовательность чисел действительных чисел a1, a2, ..., an. Заменить все ее члены,
+     больше данного Z, этим числом. Подсчитать количество замен.
+     */
+    public static int task2() {
+        int replacementCounter = 0;
+        Scanner sequenceLenght = new Scanner(System.in);
+        System.out.println("Task 2: Задайте длину последовательности чисел ");
+        int n = sequenceLenght.nextInt();
+        double[] seqofnumbers;
+        seqofnumbers = new double[n];
+        Random random = new Random();
+
+        for (int i = 0; i < n; i++) {
+            seqofnumbers[i] = random.nextInt(2500);
+        }
+
+        Scanner compared = new Scanner(System.in);
+        System.out.println("Task 2: Введите число для сравнения ");
+        int Z = compared.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            double a = seqofnumbers[i];
+            if (a > Z) {
+                seqofnumbers[i] = Z;
+                replacementCounter += 1;
+            }
+        }
+        return replacementCounter;
+    }
+
+    /*
+     Задание 3:
+     Дан массив действительных чисел, размерность которого N. Подсчитать, сколько в нём
+     отрицательных, положительных и нулевых значений.
+     */
+    public static String task3() {
+        Scanner arrayLenght = new Scanner(System.in);
+        System.out.println("Task 3: Введите длину массива ");
+        int N = arrayLenght.nextInt();
+
+        double[] arrayofnumbers;
+        arrayofnumbers = new double[N];
+        Random random = new Random();
+        for (int i = 0; i < N; i++) {
+            arrayofnumbers[i] = random.nextDouble(50 + 50) - 50;
+        }
+        int neg = 0;
+        int pos = 0;
+        int zero = 0;
+        for (int i = 0; i < N; i++) {
+            if (arrayofnumbers[i] > 0) {
+                pos += 1;
+            } else if (arrayofnumbers[i] == 0) {
+                zero += 1;
+            } else {
+                neg += 1;
+            }
+        }
+        String second = zero + " нулевых значений, " + pos + " положительных чисел";
+        return "Task 3: В данном массиве " + neg + " отрицательных чисел, " + second;
     }
 }
 
